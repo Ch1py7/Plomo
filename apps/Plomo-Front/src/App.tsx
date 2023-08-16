6645a98c415ca6eb984d13f9d746b12020d28b61
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-08-16 15:37:37 -0600

    feat: add and configure unocss and remove default styles

--- apps/Plomo-Front/src/App.tsx
+++ apps/Plomo-Front/src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FC, ReactElement, useEffect, useState } from 'react'
+import { io } from 'socket.io-client'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface TeamProps {
+  userID: number
+  userName: string
 }
 
-export default App
+export const App: FC = (): ReactElement => {
+  const [firstTeam, setFirstTeam] = useState<TeamProps[]>([])
+  const [firstTeamName, setFirstTeamName] = useState<string>('')
+  const [secondTeam, setSecondTeam] = useState<TeamProps[]>([])
+  const [secondTeamName, setSecondTeamName] = useState<string>('')
+  
+  useEffect(() => {
+    const socket = io('http://localhost:3000/')
+    socket.connect()
+    socket.on('first_team', (arg1: TeamProps[]) => {
+      setFirstTeam(arg1)
+    })
+    socket.on('first_team_name', (arg1: string) => {
+      setFirstTeamName(arg1)
+    })
+    socket.on('second_team', (arg1: TeamProps[]) => {
+      setSecondTeam(arg1)
+    })
+    socket.on('second_team_name', (arg1: string) => {
+      setSecondTeamName(arg1)
+    })
+  }, [])
+
+  return (
+    <div className='flex'>
+      <div>
+        <h1 className='text-blue'>{ firstTeamName }:</h1>
+        <ul>
+          {firstTeam.map((player) => (
+            <li key={player.userID}>{player.userName}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h1>{ secondTeamName }:</h1>
+        <ul>
+          {secondTeam.map((player) => (
+            <li key={player.userID}>{player.userName}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
